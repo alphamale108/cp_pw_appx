bb7599e93d706f2d23ea36b16173f8eedb81a528
chutpagludotnet <cluster@chutpaglu.net>
2025-06-01 09:45:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -47,9 +47,9 @@ image_list = [
 print(4321)
 bot = Client(
     "bot",
-    api_id=26797881,
-    api_hash="9699262c708c2e45ba18bfce925ed5ed",
-    bot_token="7734880223:AAGz8xu0YMFNwO2jAvcuzE-LuP2eNsBW57E"
+    api_id=21311268,
+    api_hash="af66e56cee1a95ddf34b2b5d1e422b14",
+    bot_token="7903436823:AAH0GJc4Gx1nYXccf8gDObMkbKdeaC0Xd5U"
 )
 
 @bot.on_message(filters.command(["start"]))

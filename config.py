a63193702dfb18ad289d85f3bf40b4a45e53b4ac
contentrajabot <109162597+contentrajabot@users.noreply.github.com>
2025-06-08 01:37:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "26797881"
-api_hash = "9699262c708c2e45ba18bfce925ed5ed"
-bot_token = "7734880223:AAGz8xu0YMFNwO2jAvcuzE-LuP2eNsBW57E"
+api_id = ""
+api_hash = ""
+bot_token = ""
 auth_users = [5273580528]

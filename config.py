6c6ad23d9b9f32efe852c2829ac6f913083af7fb
alphamale108 <136615536+alphamale108@users.noreply.github.com>
2025-08-02 14:27:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = ""
-api_hash = ""
-bot_token = ""
-auth_users = [5273580528]
+api_id = "20081897"
+api_hash = "8051dfd6c39c07e3eb56d58ef7f9f15f"
+bot_token = "8437726474:AAE6-hpZIu_D3KuuwcTW0q4cmLKEbnwj-Bg"
+auth_users = [7292006343]
